67ff774ca58f6e7a7f320438cf60ae515b3c786c
KrautGTI <doyle@elansolar.com>
2022-02-19 04:57:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ window.onload = function () {
 };
 var particles = Particles.init({
   selector: ".background",
-  color: ["#03dac6", "#ff0266", "#000000"],
+  color: ["#39ff14", "#ff0266", "#000000"],
   connectParticles: true,
   responsive: [
     {
       breakpoint: 768,
       options: {
-        color: ["#faebd7", "#03dac6", "#ff0266"],
+        color: ["#faebd7", "#39ff14", "#ff0266"],
         maxParticles: 43,
         connectParticles: false
       }
